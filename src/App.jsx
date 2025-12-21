b9d8cc1a878ceba411fae58c281191125c99b914
Abhishek Kumar <abhiabhishekkumar8282@gmail.com>
2025-12-21 23:09:43 +0530

    fixed some errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Research from './pages/Research';
-import Contact from './pages/Contact';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./assets/components/Navbar";
+import Footer from "./assets/components/Footer";
 
-function App() {
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Research from "./pages/Research";
+import Contact from "./pages/Contact";
+
+export default function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/research" element={<Research />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/research" element={<Research />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <Footer />
+    </>
   );
 }
-
-export default App;

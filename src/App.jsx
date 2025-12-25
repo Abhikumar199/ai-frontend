d86c9558e20e07bcb23c9d48a9b48fb455bfb6db
Abhishek Kumar <abhiabhishekkumar8282@gmail.com>
2025-12-26 02:21:08 +0530

    Fix Router config for custom domain deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Navbar from "./assets/components/Navbar";
-import Footer from "./assets/components/Footer";
-
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Research from "./pages/Research";
-import Contact from "./pages/Contact";
-
-import People from "./pages/people/People";
-import Core from "./pages/people/Core";
-import Contributors from "./pages/people/Contributors";
-import Alumni from "./pages/people/Alumni";
-
 export default function App() {
   return (
-    <Router basename="/ai-frontend">
+    <Router>
       <Navbar />
 
       <Routes>
